e260d4da40bc7b60d4327da408f30c314383bd60
usager <usager@81_64_msdn>
2019-07-08 21:25:37 -0400

    Création des methodes dans classe Categorie

--- src/main/java/mesClasseObjet/Categorie.java
+++ src/main/java/mesClasseObjet/Categorie.java
@@ -10,5 +10,48 @@ package mesClasseObjet;
  * @author usager
  */
 public class Categorie {
+    private String nomCategorie;
+    private int ageMin;
+    private int ageMax;
+
+    public Categorie() {
+    }
+
+    public Categorie(String nomCategorie, int ageMin, int ageMax) {
+        this.nomCategorie = nomCategorie;
+        this.ageMin = ageMin;
+        this.ageMax = ageMax;
+    }
+
+    public String getNomCategorie() {
+        return nomCategorie;
+    }
+
+    public void setNomCategorie(String nomCategorie) {
+        this.nomCategorie = nomCategorie;
+    }
+
+    public int getAgeMin() {
+        return ageMin;
+    }
+
+    public void setAgeMin(int ageMin) {
+        this.ageMin = ageMin;
+    }
+
+    public int getAgeMax() {
+        return ageMax;
+    }
+
+    public void setAgeMax(int ageMax) {
+        this.ageMax = ageMax;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorie{" + "nomCategorie=" + nomCategorie + ", ageMin=" + ageMin + ", ageMax=" + ageMax + '}';
+    }
+    
+    
     
 }
