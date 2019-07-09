c1c49d18e1f7320c2355abc9614e88bfb8498d16
usager <usager@81_64_msdn>
2019-07-08 21:27:18 -0400

    Création des methodes dans classe Ville

--- src/main/java/mesClasseObjet/Ville.java
+++ src/main/java/mesClasseObjet/Ville.java
@@ -10,5 +10,37 @@ package mesClasseObjet;
  * @author usager
  */
 public class Ville {
+    private String nomVille;
+    private String province;
+
+    public Ville() {
+    }
+
+    public Ville(String nomVille, String province) {
+        this.nomVille = nomVille;
+        this.province = province;
+    }
+
+    public String getNomVille() {
+        return nomVille;
+    }
+
+    public void setNomVille(String nomVille) {
+        this.nomVille = nomVille;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    @Override
+    public String toString() {
+        return "Ville{" + "nomVille=" + nomVille + ", province=" + province + '}';
+    }
+    
     
 }
