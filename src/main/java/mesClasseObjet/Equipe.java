5099a51afc37f18af3d186adeea288669df87bb9
usager <usager@81_64_msdn>
2019-07-08 21:50:42 -0400

    Correction des methodes dans classe Equipe

--- src/main/java/mesClasseObjet/Equipe.java
+++ src/main/java/mesClasseObjet/Equipe.java
@@ -10,5 +10,47 @@ package mesClasseObjet;
  * @author usager
  */
 public class Equipe {
+    private String id;
+    private String nom;
+    private String niveau;
+
+    public Equipe() {
+    }
+
+    public Equipe(String id, String nom, String niveau) {
+        this.id = id;
+        this.nom = nom;
+        this.niveau = niveau;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(String niveau) {
+        this.niveau = niveau;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipe{" + "id=" + id + ", nom=" + nom + ", niveau=" + niveau + '}';
+    }
+    
     
 }
