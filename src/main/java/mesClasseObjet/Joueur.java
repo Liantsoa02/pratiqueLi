cd95861df807499c75edf30ebe77d0513f744148
usager <usager@81_64_msdn>
2019-07-08 21:42:11 -0400

    Création des methodes dans classe Joueur

--- src/main/java/mesClasseObjet/Joueur.java
+++ src/main/java/mesClasseObjet/Joueur.java
@@ -5,10 +5,33 @@
  */
 package mesClasseObjet;
 
+import java.util.Date;
+
 /**
  *
  * @author usager
  */
 public class Joueur {
+    private Date dateNaissance;
+
+    public Joueur() {
+    }
+
+    public Joueur(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    @Override
+    public String toString() {
+        return "Joueur{" + "dateNaissance=" + dateNaissance + '}';
+    }
     
 }
